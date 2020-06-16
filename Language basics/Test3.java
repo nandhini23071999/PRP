657e35bc4363fc5ecf7edf2c6ff6ad122966435c
nandhini23071999 <66859117+nandhini23071999@users.noreply.github.com>
2020-06-16 07:14:29 -0400

    Add files via upload

---
+++ Language basics/Test3.java
@@ -0,0 +1,13 @@
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[1]);
+		int sum=a+b;
+	     System.out.println("The sum of "+ a +" and "+ b +" is "+sum);
+		
+	}
+
+}
